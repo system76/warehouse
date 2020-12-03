485bd344ccffa9b1f0d18ca8a8c32d7e5974b007
Sean Callan <sean@system76.com>
2020-12-03 15:26:31 -0700

    Remove unused enum

--- lib/warehouse/ecto_enums.ex
+++ lib/warehouse/ecto_enums.ex
@@ -2,8 +2,6 @@ import EctoEnum
 
 defenum(AreaEnum, :area, [:assembly, :receiving, :shipped, :shipping, :storage, :transit])
 
-defenum(PayableAccountType, :payable_account_type, [:net_0_payable, :net_30_payable, :net_45_payable, :net_60_payable])
-
 defenum(SkuKindEnum, :kind, [
   :accessory,
   :apparel_and_bags,
