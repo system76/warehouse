0fff38ee647e022407c8c26bb260e5d7f5bfda13
Blake Kostner <btkostner@gmail.com>
2021-09-14 11:50:51 -0600

    reset Logger metadata on new broadway message

--- lib/warehouse/broadway.ex
+++ lib/warehouse/broadway.ex
@@ -30,6 +30,8 @@ defmodule Warehouse.Broadway do
   @impl true
   @decorate trace(service: :warehouse, type: :function)
   def handle_message(_, %Message{data: data} = message, _context) do
+    Logger.reset_metadata()
+  
     bottle =
       data
       |> URI.decode()
