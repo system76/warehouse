32529057c5159c131e55f80912a7d493945a68f5
Blake Kostner <git@btkostner.io>
2021-09-30 17:38:21 -0600

    broadcast sku quantity changes

--- lib/warehouse/events.ex
+++ lib/warehouse/events.ex
@@ -5,7 +5,7 @@ defmodule Warehouse.Events do
 
   require Logger
 
-  alias Bottle.Inventory.V1.{ComponentAvailabilityUpdated}
+  alias Bottle.Inventory.V1.{ComponentAvailabilityUpdated, SkuDetailsUpdated}
 
   @callback broadcast_component_quantities(String.t(), map()) :: :ok
   @callback broadcast_sku_quantities(String.t(), map()) :: :ok
@@ -23,7 +23,16 @@ defmodule Warehouse.Events do
     Bottle.publish(message, source: @source)
   end
 
-  def broadcast_sku_quantities(_sku_id, _quantities) do
-    :ok
+  def broadcast_sku_quantities(sku_id, quantities) do
+    message =
+      SkuDetailsUpdated.new(
+        request_id: Bottle.RequestId.write(:queue),
+        sku: %{id: to_string(sku_id)},
+        available_quantity: Map.get(quantities, :available, 0),
+        demand_quantity: Map.get(quantities, :demand, 0),
+        excess_quantity: Map.get(quantities, :excess, 0)
+      )
+
+    Bottle.publish(message, source: @source)
   end
 end
