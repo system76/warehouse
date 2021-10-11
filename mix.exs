41630537be78efe669227fe0a2f9d57b44e5a708
Dairon Medina Caro <dairon.medina@gmail.com>
2021-10-11 11:29:01 -0400

    Supress warnings related to eex not included as dependency

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Warehouse.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:eex, :logger],
       mod: {Warehouse.Application, []}
     ]
   end
