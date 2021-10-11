c5993098cd65652070b891a4627147549b5e04f0
Dairon Medina Caro <dairon.medina@gmail.com>
2021-10-11 16:31:40 -0400

    Update new Movement schema to match table structure

--- lib/warehouse/schemas/movement.ex
+++ lib/warehouse/schemas/movement.ex
@@ -3,15 +3,22 @@ defmodule Warehouse.Schemas.Movement do
 
   import Ecto.Changeset
 
-  alias Warehouse.Schemas.{Location, Part}
+  alias Warehouse.Schemas.Location
+  alias Warehouse.Schemas.Part
 
   @type t :: %__MODULE__{
-          location: Location.t(),
+          from_location: Location.t(),
+          to_location: Location.t(),
           part: Part.t()
         }
 
-  schema "inventory_part_movements" do
-    belongs_to :location, Location
+  @required_fields ~w(to_location part_id)a
+  @optional_fields ~w(from_location)a
+  @fields @required_fields ++ @optional_fields
+
+  schema "inventory_movements" do
+    belongs_to :from_location, Location
+    belongs_to :to_location, Location
     belongs_to :part, Part
 
     timestamps(updated_at: false)
@@ -19,8 +26,8 @@ defmodule Warehouse.Schemas.Movement do
 
   def changeset(part, attrs) do
     part
-    |> cast(attrs, [:location_id, :part_id])
-    |> validate_required([:location_id, :part_id])
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
     |> assoc_constraint(:sku)
     |> assoc_constraint(:location)
   end
