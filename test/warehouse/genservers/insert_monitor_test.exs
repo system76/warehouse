5b0f546a8876105f65b35cd3941f9acfe79f8d33
Dairon Medina Caro <dairon@system76.com>
2021-11-04 15:32:43 -0400

    Add tests for handling insertion

--- test/warehouse/genservers/insert_monitor_test.exs
+++ test/warehouse/genservers/insert_monitor_test.exs
@@ -39,4 +39,32 @@ defmodule Warehouse.GenServers.InsertMonitorTest do
       assert_receive {:trace, ^pid, :return_to, {:gen_server, :try_dispatch, 4}}
     end
   end
+
+  describe "handle_info/2" do
+    setup context do
+      stub(Warehouse.Clients.Assembly.Mock, :request_component_demands, fn -> [] end)
+      start_supervised!({InsertMonitor, [name: context.test, fetch_interval: :timer.seconds(1)]})
+      :ok
+    end
+
+    test "fetches newly inserted SKUs and starts their processes" do
+      0 = Warehouse.SkuSupervisor |> DynamicSupervisor.which_children() |> length()
+
+      insert_list(5, :sku)
+      # Wait a bit over the configured fetch_interval
+      Process.sleep(:timer.seconds(2))
+
+      assert Warehouse.SkuSupervisor |> DynamicSupervisor.which_children() |> length() == 5
+    end
+
+    test "fetches newly inserted Components and starts their processes" do
+      0 = Warehouse.ComponentSupervisor |> DynamicSupervisor.which_children() |> length()
+
+      insert_list(3, :component)
+      # Wait a bit over the configured fetch_interval
+      Process.sleep(:timer.seconds(2))
+
+      assert Warehouse.ComponentSupervisor |> DynamicSupervisor.which_children() |> length() == 3
+    end
+  end
 end
